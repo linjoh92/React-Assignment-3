580d8d70510179b189c5969eaed8b987c9f78a15
Linn Johansson <linn.johansson2@edu.cmeducations.se>
2023-02-17 12:11:19 +0100

    responsive css

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -7,11 +7,13 @@ const Counter = (props) => {
   const { days, hours, minutes, seconds } = props;
 
   const itIsFriday = days === 6 ? '0 days left' : "";
+  const fridayClass = days === 6 ? 'friday-class' : "";
   const hideOrShow = days === 6 ? 'hide' : 'show';
   const dayOrDays = days === 1 ? 'day' : 'days';
 
   return (
-    <div className='counter-container'>{itIsFriday}
+    
+    <div className= {`counter-container ${fridayClass}`}>{itIsFriday}
         <p className= {`bold-time ${hideOrShow}`}>{days} {dayOrDays}</p>       
         <p className= {`bold-time ${hideOrShow}`}>{hours} hrs</p> 
         <p className= {hideOrShow}>{minutes} min</p> 
