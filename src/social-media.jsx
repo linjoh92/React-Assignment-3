2cf86c5956636a35cb9f974705ba3ec48256022b
Linn Johansson <linn.johansson2@edu.cmeducations.se>
2023-02-18 13:06:38 +0100

    link instgram

--- src/social-media.jsx
+++ src/social-media.jsx
@@ -8,9 +8,9 @@ import youtube from './assets/youtube.svg'
 const SocialMedia = () => {
   return (
   <div className='socialamedia-icons'>
-    <img src={facebook} alt="facebook-icon" className='icon' height='18px'/>
-    <img src={instagram} alt="instagram-icon" className='icon' height='18px'/>
-    <img src={youtube} alt="youtube-icon" className='icon' height='18px'/>
+    <a href="https://www.instagram.com/dayslefttofriday/" target="_blank"><img src={facebook} alt="facebook-icon" className='icon' height='18px'/></a>
+    <a href="https://www.instagram.com/dayslefttofriday/" target="_blank"><img src={instagram} alt="instagram-icon" className='icon' height='18px'/></a>
+    <a href="https://www.instagram.com/dayslefttofriday/" target="_blank"><img src={youtube} alt="youtube-icon" className='icon' height='18px'/></a>
   </div>
   )
 }
