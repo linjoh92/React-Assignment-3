1a3e06438c9bcc65a18b8de3cf3c1894c2a2f8ee
Linn Johansson <linn.johansson2@edu.cmeducations.se>
2023-02-18 12:40:49 +0100

    text update

--- src/Text-content.jsx
+++ src/Text-content.jsx
@@ -8,7 +8,7 @@ const TextContent = (props) => {
   return (
     <div className='content-container'>
         <h1 className='content-rubrik'>{itIsFriday} friday</h1>
-        <p className='content-text'>Fridays are universally loved. It's the end of the work or school week and the start of the weekend, which means leisure time, social activities, and relaxation.</p>
+        <p className='content-text'>Fridays are universally loved. It's the end of the work or school week and the start of the weekend. It's a day for unwinding, having fun, and spending time with loved ones. There's something about Fridays that puts a smile on everyone's face.</p>
     </div>
   )
 }
